348fed477f3cc57005cdcef88ee677ed9c31bd27
Wendy Harris <wendyv510@hotmail.com>
2019-10-13 19:20:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,11 +9,13 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-   
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
     array.sort
 end
 
 def using_reverse(array)
+   
     array.reverse
 end
 
