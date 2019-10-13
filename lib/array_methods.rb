1e7b4787e08102a77d91e3a80f6fa0428c74a82f
Wendy Harris <wendyv510@hotmail.com>
2019-10-13 19:21:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,11 +21,13 @@ def using_reverse(array)
 end
 
 def using_first(array)
-   
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
     array(first)
 end
 
 def using_last(array)
+   
     array(last)
 end
 
